b4677739760f0cdabc6ea2522e7a34661e827e7c
felipezzz1 <felipezorzo78@gmail.com>
2024-09-30 21:04:40 -0300

    first steps, creating the class movie

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Movie myMovie = new Movie();
+
+        myMovie.name = "Batman";
+        myMovie.yearOfRelease = 1990;
+        myMovie.duration = 165;
+
+        System.out.println(myMovie.name);
+    }
+}
---
+++ src/Movie.java
@@ -0,0 +1,9 @@
+public class Movie {
+    String name;
+    int yearOfRelease;
+    boolean isIncludedInPlan;
+    double rating;
+    int numberOfRatings;
+    int duration;
+
+}
