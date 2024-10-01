e1159ab4c883ecbfea7bd5d90c56723852d8a6f2
felipezzz1 <felipezorzo78@gmail.com>
2024-09-30 21:49:41 -0300

    getters and setters

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
+import br.com.fezor.screenmatch.models.Movie;
+
 public class Main {
     public static void main(String[] args) {
         Movie myMovie = new Movie();
 
-        myMovie.name = "Batman";
-        myMovie.yearOfRelease = 1990;
-        myMovie.duration = 165;
+        myMovie.setName("Batman");;
+        myMovie.setYearOfRelease(1990);;
+        myMovie.setDuration(165);
 
-        myMovie.listMovie();
         myMovie.rateMovie(9.9);
         myMovie.rateMovie(8);
         myMovie.rateMovie(10);
-        myMovie.listMovie();
+        System.out.println(myMovie.getNumberOfRatings());
     }
 }
---
+++ src/br/com/fezor/screenmatch/models/Movie.java
@@ -0,0 +1,71 @@
+package br.com.fezor.screenmatch.models;
+
+public class Movie {
+    private String name;
+    private int yearOfRelease;
+    boolean isIncludedInPlan;
+    private double totalRating;
+    private int numberOfRatings;
+    private int duration;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public boolean isIncludedInPlan() {
+        return isIncludedInPlan;
+    }
+
+    public void setIncludedInPlan(boolean includedInPlan) {
+        isIncludedInPlan = includedInPlan;
+    }
+
+    public double getTotalRating() {
+        return totalRating;
+    }
+
+    public int getNumberOfRatings() {
+        return numberOfRatings;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void listMovie(){
+        System.out.println("br.com.fezor.screenmatch.models.Movie name: " + name);
+        System.out.println("Year of release: " + yearOfRelease);
+        System.out.println("Duration: " + duration);
+    }
+
+    public void rateMovie(double review){
+        totalRating += review;
+        numberOfRatings++;
+    }
+
+    public double averageRating(){
+        double media = totalRating/numberOfRatings;
+        if(numberOfRatings == 0){
+            return 0;
+        }else{
+            return totalRating/numberOfRatings;
+        }
+    }
+
+}
