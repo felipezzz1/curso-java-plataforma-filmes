ea25881cdfb61183b896640c3fb7b96ecf3c5cc9
felipezzz1 <felipezorzo78@gmail.com>
2024-10-03 16:16:43 -0300

    using list and saving the movies from api

--- src/br/com/fezor/screenmatch/main/MainWithSearch.java
+++ src/br/com/fezor/screenmatch/main/MainWithSearch.java
@@ -7,11 +7,14 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainWithSearch {
@@ -19,41 +22,60 @@ public class MainWithSearch {
         // not a best practice, just for example
         String API_KEY = "dd8a022c";
         Scanner read = new Scanner(System.in);
-        System.out.println("Type the movie you want to search: ");
-        var search = read.nextLine();
+        String search = "";
+        List<Title> titles = new ArrayList<>();
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
 
-        String address = "https://www.omdbapi.com/?t=" + search.replace(" ", "+") + "&apikey=" + API_KEY;
-        try{
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(address))
-                    .build();
+        while(!search.equalsIgnoreCase("sair")){
+            System.out.println("Type the movie you want to search: ");
+            search = read.nextLine();
 
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+            if(search.equalsIgnoreCase("sair")){
+                break;
+            }
 
-            String json = response.body();
-            System.out.println(json);
+            String address = "https://www.omdbapi.com/?t=" + search.replace(" ", "+") + "&apikey=" + API_KEY;
+            try{
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(address))
+                        .build();
 
-            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                    .create();
-            TitleOmdb myTitle = gson.fromJson(json, TitleOmdb.class);
-            System.out.println(myTitle);
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                System.out.println(json);
+
+                TitleOmdb myTitle = gson.fromJson(json, TitleOmdb.class);
+                System.out.println(myTitle);
 
 
-            Title myTitle1 = new Title(myTitle);
-            System.out.println(myTitle1);
-        }catch (NumberFormatException e){
-            System.out.println("An error occurred: ");
-            System.out.println(e.getMessage());
-        }catch (IllegalArgumentException e){
-            System.out.println("An error ocurred in the search, please check the address: ");
-            System.out.println(e.getMessage());
-        }catch (InvalidYearException e){
-            System.out.println(e.getMessage());
+                Title myTitle1 = new Title(myTitle);
+                System.out.println(myTitle1);
+
+                titles.add(myTitle1);
+
+            }catch (NumberFormatException e){
+                System.out.println("An error occurred: ");
+                System.out.println(e.getMessage());
+            }catch (IllegalArgumentException e){
+                System.out.println("An error ocurred in the search, please check the address: ");
+                System.out.println(e.getMessage());
+            }catch (InvalidYearException e){
+                System.out.println(e.getMessage());
+            }
+
         }
 
-        System.out.println("The program run correctly");
+        System.out.println(titles);
 
+        FileWriter write = new FileWriter("movies.txt");
+        write.write(gson.toJson(titles));
+        write.close();
+        System.out.println("The program run correctly");
     }
 }
